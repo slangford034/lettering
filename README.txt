eec03b02602361151316b95b00d6c7a05b93a41b
Steven <slangford@engr-131-06.eastern.ewu.edu>
2016-04-07 14:12:29 -0700

    Version 1

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/slangford034/pen/JXMJQK.
+
+ 
---
+++ js/index.js
@@ -0,0 +1,7 @@
+// Selector Action Option
+
+// Title
+$('#title').lettering();
+
+// Poster
+$('h3').lettering('lines');
